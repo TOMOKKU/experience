abd360403348e051a1119ce62cb14bcf75b3a1cb
TOMOKKU <69345378+TOMOKKU@users.noreply.github.com>
2022-04-21 17:11:51 +0900

    avoid rooping forever

--- thermo.py
+++ thermo.py
@@ -1,13 +1,19 @@
 from picamera import PiCamera
 from time import sleep
+import numpy as np
+import matplotlib.pyplot as plt
 
 camera = PiCamera()
 
+camera.resolution = (720, 480)
 camera.start_preview()
 sleep(5)
+i = 0
 while True:
     try:
-        camera.capture('home/pi/toki/images1/image%s.jpg' % i)
+        i += 1
+        camera.capture('/home/pi/toki/images1/image%s.jpg' % i)
         sleep(60)
     except KeyboardInterrupt:
-        camera.stop_preview()
+        break
+camera.stop_preview()
